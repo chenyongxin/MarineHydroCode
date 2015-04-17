3321caee2bde11b21cd245c98e376903ef8d72b6
Yongxin Chen <yc11e14@JFB36-LVL05-W03.soton.ac.uk>
2015-04-17 21:36:57 +0100

    1

--- Assignment.py
+++ Assignment.py
@@ -11,5 +11,15 @@ from LiftBody import solve_gamma_kutta, make_jukowski,make_circle
 # 1. A circular cylinder
 # 1.1 N = 32
 
-circle = make_circle(32)
+circle = make_circle(128)
+solve_gamma_kutta(circle)
 
+#first of all, define the coordinate of circle, then use these coordinates to find the velocity.
+#directly use flow_velocity to find the velocity along the surface
+
+def pressure(panels):
+    x = numpy.array([p.xc for p in panels])
+    y = numpy.array([p.yc for p in panels])
+    u,v = flow_velocity(panels,x,y)
+    return 2*numpy.sum( numpy.multiply([1-x**2-y**2],[p.S for p in panels])) 
+print 'C_P = ', pressure(circle)
