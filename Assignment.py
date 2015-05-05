781c68fd13731c965763091569be02b4e2af58ac
chenyongxin <yongxinchen11@gmail.com>
2015-05-05 21:12:28 +0100

    5.5.2015

--- Assignment.py
+++ Assignment.py
@@ -92,24 +92,20 @@ def CPplot(panels):
 
 #from BoundaryLayer import ddx_delta, heun, g_pohl, g_1, df_0, march
 #lam0 = 7.05232310118
+
+#from BoundaryLayer import ddx_delta, heun, g_pohl, g_1, df_0, march
+#lam0 = 7.05232310118
+
 #
+
+# solve the boundary thickness delta
+
+
+
+
 #
-## ODE solver, like march()
-#def findDelta(x,u_e,du_e,nu):
-#    lam0 = 7.05232310118
-#    delta0 = numpy.sqrt(lam0*nu/du_e[0])                # set delta0
-#    delta = numpy.full_like(x,delta0)                   # delta array
-#    lam = numpy.full_like(x,lam0)                       # lambda array
-#    
-#    for i in range(len(x)-1):                           # march!
-#        delta[i+1] = heun(g_pohl,delta[i],i,x[i+1]-x[i],# integrate BL using...
-#                             u_e,du_e,nu)
-#        lam[i+1] = delta[i+1]**2*du_e[i+1]/nu 
-#    return delta,lam
-#
-## solve the boundary thickness delta
 #nu = 1e-5                                   # viscosity, based on Reynolds number, nu = UR/Re
-#N = 32                                     # number of steps
+#N = 128                                     # number of steps
 #s = numpy.linspace(0,numpy.pi,N)            # distance goes from 0..pi
 #u_e = 2.*numpy.sin(s)                       # velocity
 #du_e = 2.*numpy.cos(s)                      # gradient
@@ -138,9 +134,9 @@ def CPplot(panels):
 #    return half_c_f(lam, nu, delta, u_e)*u_e**2
 #    
 ##print tau_w(lam[0], nu, delta[0], u_e[0])
-#tau = numpy.full_like(delta, 0)
-#for i in range(iSep+1):
-#    tau[i] = tau_w(lam[i], nu, delta[i], u_e[i])
+##tau = numpy.full_like(delta, 0)
+##for i in range(iSep+1):
+##    tau[i] = tau_w(lam[i], nu, delta[i], u_e[i])
 #
 #tau = [tau_w(lam[i], nu, delta[i], u_e[i]) for i in range(N)]
 #
@@ -148,8 +144,8 @@ def CPplot(panels):
 #sx = numpy.sin(s[0:iSep+1])
 #
 #def drag(tau, sx, N):
-##    return numpy.sum(tau_w[:iSep]*sx*numpy.pi/N)
-#    return numpy.trapz(tau[:iSep+1]*sx, dx = numpy.pi/(N-1))
+#    return numpy.sum(tau[:iSep+1]*sx*numpy.pi/(N-1))
+##    return numpy.trapz(tau[:iSep+1]*sx, dx = numpy.pi/(N-1))
 #
 #
 ##print(sx[0:iSep])
@@ -167,6 +163,70 @@ def CPplot(panels):
 #pyplot.ylabel(r'$\tau_w s_x$', size=20)
 
 
+
+
+
+
+
+
+
+#######
+# solve the boundary thickness delta
+#nu = 1e-5                                   # viscosity, based on Reynolds number, nu = UR/Re
+#N = 64                                     # number of steps
+#s = numpy.linspace(0,numpy.pi,N)            # distance goes from 0..pi
+#u_e = 2.*numpy.sin(s)                       # velocity
+#du_e = 2.*numpy.cos(s)                      # gradient
+#delta, lam, iSep = march(s,u_e,du_e,nu)     # solve!
+#tau = numpy.full_like(delta, 0)
+#for i in range(iSep+1):
+#    tau[i] = tau_w(lam[i], nu, delta[i], u_e[i])
+#
+#sx = numpy.sin(s[0:iSep])
+#print 'drag coefficient = ', 2*drag(tau, sx, N)/numpy.pi
+#
+#C_F_flat = 1.33*numpy.sqrt(nu/s[-1])
+#print("Flate plate: "+ '%0.2e' %C_F_flat) 
+########
+#
+#
+##tau = nu*1000 *u_e/delta*df_0(lam)
+#
+## compute frictional drag. int tau ds, where ds is the lenght of vortex panel
+#
+##print 'drag coefficient = ', drag(tau,circle)/(1./2.*1000*1*numpy.pi)
+#
+#
+#
+#
+##def c_f(lam, nu, delta, u_e):
+##    Re_d = delta*u_e/nu
+##    return 2*df_0(lam)/Re_d
+#    
+#def half_c_f(lam, nu, delta, u_e):
+#    Re_d = delta*u_e/nu
+#    return df_0(lam)/Re_d
+#    
+#
+#def tau_w(lam, nu, delta, u_e):
+#    if u_e == 0: return 0
+#    return half_c_f(lam, nu, delta, u_e)*u_e**2
+#    
+##print tau_w(lam[0], nu, delta[0], u_e[0])
+#
+#
+#
+#def drag(tau_w, sx, N):
+##    return numpy.sum(tau_w[:iSep]*sx*numpy.pi/N)
+#    return numpy.trapz(tau_w[:iSep]*sx, dx = numpy.pi/N)
+
+
+#print(sx[0:iSep])
+# Notice: We just only compute half body, so in the end, we need to times 2 to get the whole cylinder's friction
+
+
+
+
 #--------------------------
 #|    This is 2.1         |
 #--------------------------
@@ -175,7 +235,7 @@ def CPplot(panels):
 #
 #
 #N1, N2, N3 = 32, 64, 128    # define different resolution
-#foil1, foil2, foil3 = make_jukowski(N1), make_jukowski(N2), make_jukowski(N3)   # initialize foils with three resolutions
+#foil1, foil2, foil3 = make_jukowski(N1，0.15), make_jukowski(N2，0.15), make_jukowski(N3，0.15)   # initialize foils with three resolutions
 #n = 10                                # this number is the division of AoA
 #alpha = numpy.linspace(0,10,n)         # set angle of attack which is a set of number from 0 to 10**0
 ## define a loop to calculate lift coefficient
@@ -205,31 +265,120 @@ def CPplot(panels):
 #--------------------------
 #|    This is 2.2         |
 #--------------------------
-#from Separation import split_panels, solve_plot_boundary_layers
-#
-#def new_predict_jukowski_separation(t_c,alpha=0,N=128):
-#    # set dx to gets the correct t/c
-#    foil = make_jukowski(N,dx=t_c-0.019)
-#
-#    # find and print t/c
-#    x0 = foil[N/2].xc
-#    c = foil[0].xc-x0
-#    t = 2.*numpy.max([p.yc for p in foil])
-#    
-#
-#    # solve potential flow and boundary layer evolution
-#    solve_gamma_kutta(foil,alpha)
-#    top,bottom = solve_plot_boundary_layers(foil,alpha)
-#    return (top.x_sep-x0)/c
+from Separation import split_panels, Pohlhausen
+
+def new_solve_plot_boundary_layers(panels,alpha=0,nu=1e-5):
+    # split the panels
+    top_panels,bottom_panels = split_panels(panels)
+    
+    # Set up and solve the top boundary layer
+    top = Pohlhausen(top_panels,nu)
+    top.march()
+
+    # Set up and solve the bottom boundary layer
+    bottom = Pohlhausen(bottom_panels,nu)
+    bottom.march()
+    return top,bottom
+
+def new_predict_jukowski_separation(t_c,alpha=0,N=128):
+    # set dx to gets the correct t/c
+    foil = make_jukowski(N,dx=t_c-0.019)
+
+    # find and print t/c
+    x0 = foil[N/2].xc
+    c = foil[0].xc-x0
+    t = 2.*numpy.max([p.yc for p in foil])
+    # solve potential flow and boundary layer evolution
+    solve_gamma_kutta(foil,alpha)
+    top, bottom = new_solve_plot_boundary_layers(foil,alpha)
+    return (top.x_sep-x0)/c, (bottom.x_sep-x0)/c
 #
 #
 #n = 10                                # this number is the division of AoA
-#alpha = numpy.linspace(0,1,n) 
-#sep = numpy.empty(n)
+#alpha = numpy.linspace(0,10,n) 
+#sep = numpy.empty([n,2])                # Initialize a sep matrix to contain the separation points information.
 #for i in range(n):
-#    sep[i] = new_predict_jukowski_separation(0.2,alpha[i])
+#    sep[i,:] = new_predict_jukowski_separation(0.15,alpha[i]*numpy.pi/180)
 #pyplot.figure(figsize=(8,6))
 #pyplot.ylabel(r'Separation position x/c from the leading edge',fontsize=16)
-#pyplot.xlabel(r'AoA $\alpha (rad)$',fontsize=16)
-#pyplot.plot(alpha,sep,lw=3, c='c', label='Separation')
-#pyplot.legend(loc='lower right')
+#pyplot.xlabel(r'AoA $\alpha (degree)$',fontsize=16)
+#pyplot.plot(alpha,sep[:,0],lw=3, c='r', label='Top Separation')
+#pyplot.scatter(alpha, sep[:,0])
+#pyplot.plot(alpha,sep[:,1],lw=3, c='b', label='Bottom Separation')
+#pyplot.scatter(alpha, sep[:,1])
+#pyplot.legend(loc='upper left')
+#
+#for i in range(n):
+#    print 'AoA: '+ '%.3f' %(alpha[i]*numpy.pi/180) + ' degree, Top Separation at x/c= ' + '%.3f'%(sep[i,0])+ ', Bottom Separation at x/c= ' + '%.3f'%(sep[i,1])
+
+
+
+
+
+
+#--------------------------
+#|    This is 3           |
+#--------------------------
+
+def naca_offset(x): 
+    return 5*0.15*(0.2969*numpy.sqrt(x)-0.1260*x-0.3516*x**2+0.2843*x**3-0.1036*x**4)
+
+def make_naca(N):
+    theta = numpy.linspace(0,-2.*numpy.pi,N+1)
+    x = (numpy.cos(theta)+1)/2.
+    y = naca_offset(x)*numpy.sign(numpy.sin(theta))
+    naca = numpy.empty(N, dtype=object)
+    for i in range(N):
+        naca[i] = Panel(x[i], y[i], x[i+1], y[i+1])
+    return naca
+    
+
+def predict_naca_separation(N,alpha=0):
+    # set dx to gets the correct t/c
+    naca = make_naca(N)
+
+    x0 = naca[N/2].xc
+    c = naca[0].xc-x0
+    # solve potential flow and boundary layer evolution
+    solve_gamma_kutta(naca,alpha)
+    top, bottom = new_solve_plot_boundary_layers(naca,alpha)
+    return (top.x_sep-x0)/c, (bottom.x_sep-x0)/c
+
+N = 128
+n = 10
+alpha = numpy.linspace(0,10,n) 
+sep = numpy.empty([n,2])                # Initialize a sep matrix to contain the separation points information.
+for i in range(n):
+    sep[i,:] = predict_naca_separation(N,alpha[i]*numpy.pi/180)
+
+pyplot.figure(figsize=(8,6))
+pyplot.ylabel(r'Separation position x/c from the leading edge',fontsize=16)
+pyplot.xlabel(r'AoA $\alpha (degree)$',fontsize=16)
+pyplot.plot(alpha,sep[:,0],lw=3, c='r', label='Top Separation')
+pyplot.scatter(alpha, sep[:,0])
+pyplot.plot(alpha,sep[:,1],lw=3, c='b', label='Bottom Separation')
+pyplot.scatter(alpha, sep[:,1])
+pyplot.legend(loc='upper left') 
+
+naca = make_naca(N)
+alpha = numpy.pi/16
+solve_gamma_kutta(naca,alpha)
+plot_flow(naca,alpha)
+
+#Compare with jukouski foil
+n = 10                                # this number is the division of AoA
+alpha = numpy.linspace(0,10,n) 
+sep2 = numpy.empty([n,2])                # Initialize a sep matrix to contain the separation points information.
+for i in range(n):
+    sep2[i,:] = new_predict_jukowski_separation(0.15,alpha[i]*numpy.pi/180)
+
+
+
+pyplot.figure(figsize=(8,6))
+pyplot.ylabel(r'Separation position x/c from the leading edge',fontsize=16)
+pyplot.xlabel(r'AoA $\alpha (degree)$',fontsize=16)
+pyplot.plot(alpha,sep[:,0],lw=3, c='r', label='NACA Top Separation')
+pyplot.plot(alpha,sep2[:,0],lw=3, c='g', label='Jukowski Top Separation')
+pyplot.plot(alpha,sep[:,1],lw=3, c='b', label='NACA Bottom Separation')
+pyplot.plot(alpha,sep2[:,1],lw=3, c='y', label='Jukowski Bottom Separation')
+pyplot.legend(loc='bottom left') 
